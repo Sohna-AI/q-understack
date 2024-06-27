c29e99e31f621694565681c2e16392fe72c2f5a6
Ocean Briggs-Mineart <Forestbriggs14@gmail.com>
2024-06-26 21:30:39 -0700

    simplified user_id check in user_questions route

--- app/api/question_routes.py
+++ app/api/question_routes.py
@@ -19,7 +19,7 @@ def user_questions():
     Query for all of the current users questions and returns them in
     a list of question dictionaries
     """
-    user_id = current_user.get_id()
-    # user_id = 1 #! Hard coded in for testing without login, remove once unneeded
-    questions = Question.query.filter(Question.user.has(id=user_id))
+    # user_id = current_user.get_id()
+    user_id = 1 #! Hard coded in for testing without login, remove once unneeded
+    questions = Question.query.filter(Question.user_id==user_id)
     return {'questions': [question.to_dict() for question in questions] }
