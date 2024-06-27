ec3158a2a3714ea359f750119f3ea15ec5fe3205
Ocean Briggs-Mineart <Forestbriggs14@gmail.com>
2024-06-26 21:47:38 -0700

    added add_prefix_for_prod to needed foreign keys and join tables

--- app/models/question.py
+++ app/models/question.py
@@ -1,10 +1,13 @@
-from .db import db, environment, SCHEMA
+from .db import db, environment, SCHEMA, add_prefix_for_prod
 from sqlalchemy.sql import func
 
 question_tag = db.Table('question_tag',
     db.Column('question_id', db.Integer, db.ForeignKey('questions.id'), primary_key=True),
     db.Column('tag_id', db.Integer, db.ForeignKey('tags.id'), primary_key=True),
     )
+
+if environment == 'production':
+    question_tag.schema = SCHEMA
 
 
 class Question(db.Model):
@@ -14,7 +17,7 @@ class Question(db.Model):
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
     title = db.Column(db.String(50), nullable=False)
     details = db.Column(db.String(255), nullable=False)
     expectation = db.Column(db.String(255), nullable=False)
