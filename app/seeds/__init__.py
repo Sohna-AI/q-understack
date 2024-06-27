3f290f32e767854d86009422eca865da9bb99aeb
Ocean Briggs-Mineart <Forestbriggs14@gmail.com>
2024-06-26 22:17:54 -0700

    changed undo seeds order, added SCHEMA to migrations for production

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -19,10 +19,10 @@ def seed():
         # command, which will  truncate all tables prefixed with 
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
-        undo_users()
-        undo_questions()
-        undo_answers()
         undo_comments()
+        undo_answers()
+        undo_questions()
+        undo_users()
     seed_users()
     seed_questions()
     seed_answers()
@@ -33,8 +33,8 @@ def seed():
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
-    undo_users()
-    undo_questions()
-    undo_answers()
     undo_comments()
+    undo_answers()
+    undo_questions()
+    undo_users()
     # Add other undo functions here
