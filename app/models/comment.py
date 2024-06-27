5e8cef7386c3c7c755502a1b3e2b2d25fc50156e
Ocean Briggs-Mineart <Forestbriggs14@gmail.com>
2024-06-26 21:47:38 -0700

    added add_prefix_for_prod to needed foreign keys and join tables

--- app/models/comment.py
+++ app/models/comment.py
@@ -1,4 +1,4 @@
-from .db import db, environment, SCHEMA
+from .db import db, environment, SCHEMA, add_prefix_for_prod
 from .question import Question
 from .answer import Answer
 from sqlalchemy.sql import func
@@ -12,7 +12,7 @@ class Comment(db.Model):
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
     type_id = db.Column(db.Integer, nullable=False)
     type = db.Column('type', db.String(20), nullable=False)
     comment = db.Column(db.String(255), nullable=False)
