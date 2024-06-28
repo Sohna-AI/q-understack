426dacc6aa6ba501047344f180fe5136deda2c3f
Ocean Briggs-Mineart <Forestbriggs14@gmail.com>
2024-06-28 14:59:48 -0700

    refactored error messages and responses to not use jsonify because flask auto handles json responses for dicts and lists, created saved questions and question details routes, fixed QuestionForm validators, started saves seeder;

--- app/forms/create_question_form.py
+++ app/forms/create_question_form.py
@@ -3,7 +3,7 @@ from wtforms.fields import StringField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired, Length
 
 class QuestionForm(FlaskForm):
-    title = StringField('Title', Length(max=50), validators=[DataRequired()])
-    details = TextAreaField('Details', Length(max=1500), validators=[DataRequired()])
-    expectation = TextAreaField('Expectation', Length(max=1500), validators=[DataRequired()])
+    title = StringField('Title', validators=[DataRequired(), Length(max=50)])
+    details = TextAreaField('Details', validators=[DataRequired(), Length(max=1500)])
+    expectation = TextAreaField('Expectation', validators=[DataRequired(), Length(max=1500)])
     submit = SubmitField('Submit')
--- react-vite/src/components/LoginFormModal/LoginFormModal.jsx
+++ react-vite/src/components/LoginFormModal/LoginFormModal.jsx
@@ -5,57 +5,57 @@ import { useModal } from "../../context/Modal";
 import "./LoginForm.css";
 
 function LoginFormModal() {
-  const dispatch = useDispatch();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [errors, setErrors] = useState({});
-  const { closeModal } = useModal();
+    const dispatch = useDispatch();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [errors, setErrors] = useState({});
+    const { closeModal } = useModal();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+    const handleSubmit = async (e) => {
+        e.preventDefault();
 
-    const serverResponse = await dispatch(
-      thunkLogin({
-        email,
-        password,
-      })
+        const serverResponse = await dispatch(
+            thunkLogin({
+                email,
+                password,
+            })
+        );
+
+        if (serverResponse) {
+            setErrors(serverResponse);
+        } else {
+            closeModal();
+        }
+    };
+
+    return (
+        <>
+            <h1>Log In</h1>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Email
+                    <input
+                        type="text"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                </label>
+                {errors.email && <p>{errors.email}</p>}
+                <label>
+                    Password
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                </label>
+                {errors.password && <p>{errors.password}</p>}
+                <button type="submit">Log In</button>
+            </form>
+        </>
     );
-
-    if (serverResponse) {
-      setErrors(serverResponse);
-    } else {
-      closeModal();
-    }
-  };
-
-  return (
-    <>
-      <h1>Log In</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Email
-          <input
-            type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </label>
-        {errors.email && <p>{errors.email}</p>}
-        <label>
-          Password
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </label>
-        {errors.password && <p>{errors.password}</p>}
-        <button type="submit">Log In</button>
-      </form>
-    </>
-  );
 }
 
 export default LoginFormModal;
