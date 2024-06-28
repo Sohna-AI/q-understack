5db209b839ffb7764c31ec8c12ee93974524d7ca
Ocean Briggs-Mineart <Forestbriggs14@gmail.com>
2024-06-27 20:32:41 -0700

    started answer seeder

--- app/seeds/answers.py
+++ app/seeds/answers.py
@@ -1,14 +1,46 @@
-from app.models import db, Answer, environment, SCHEMA
+from app.models import db, Answer, User, Question, environment, SCHEMA
 from sqlalchemy.sql import text
+from datetime import datetime
+
+question_answers = [
+    {
+        'title': 'Is HTML a programming language?',
+        'user_answers': [
+            {
+                'username': 'marnie', 
+                'answer': {
+                        'text': 'Si',
+                        'created_at': datetime.now()
+                    }
+            }
+        ]
+    }
+]
 
 def seed_answers():
-    demo = Answer(
-        user_id = 2,
-        question_id = 1,
-        text = 'learn more languages to get a job'
-    )
+    for info in question_answers:
+        title, user_answers = info.values()
+        question = Question.query.filter(Question.title == title).first()
+        
+        for user_info in user_answers:
+            username, answer = user_info.values()
+            
+            user = User.query.filter(User.username == username).first()
+            new_answer = Answer(
+                user_id=user.id,
+                text=answer['text'],
+                created_at=answer['created_at'],
+                updated_at=answer['created_at']
+            )
+            question.answers.append(new_answer)
     
-    db.session.add(demo)
+    # demo = Answer(
+    #     user_id = 2,
+    #     question_id = 1,
+    #     text = 'learn more languages to get a job'
+    # )
+    
+    # db.session.add(demo)
     db.session.commit()
     
 def undo_answers():
