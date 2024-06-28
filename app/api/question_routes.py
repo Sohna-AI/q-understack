1c5cb64e80409e21c915ce43e2cf4339526dae42
Ocean Briggs-Mineart <Forestbriggs14@gmail.com>
2024-06-27 20:07:18 -0700

    created seeders for user_questions

--- app/api/question_routes.py
+++ app/api/question_routes.py
@@ -13,13 +13,21 @@ def questions():
     return {'questions': [question.to_dict() for question in questions]}
 
 @question_routes.route('/current')
-# @login_required
+@login_required
 def user_questions():
     """
     Query for all of the current users questions and returns them in
     a list of question dictionaries
     """
-    # user_id = current_user.get_id()
-    user_id = 1 #! Hard coded in for testing without login, remove once unneeded
+    user_id = current_user.get_id()
+    # user_id = 1 #! Hard coded in for testing without login, remove once unneeded
     questions = Question.query.filter(Question.user_id==user_id)
     return {'questions': [question.to_dict() for question in questions] }
+
+
+# @question_routes.route('/saves')
+# @login_required
+# def user_saves():
+#     """
+#     Query for all 
+#     """
