1b197844bd7e52c5af7a124c8fd12d00135b76b2
Ocean Briggs-Mineart <Forestbriggs14@gmail.com>
2024-06-29 16:34:22 -0700

    created Sidebar component

--- react-vite/src/components/Sidebar/Siderbar.jsx
+++ react-vite/src/components/Sidebar/Siderbar.jsx
@@ -1,3 +1,17 @@
+import { FaBookmark, FaHome, FaQuestionCircle, FaSave, FaTag, } from 'react-icons/fa';
+import './Sidebar.css';
+
 export default function Sidebar() {
-    return null;
+    return (
+        <div id='sidebar'>
+            <div id='sidebar__main-links'>
+                <div><FaHome style={{ fontSize: 16 }} />Home</div>
+                <div><FaQuestionCircle style={{ fontSize: 16 }} />Questions</div>
+                <div><FaTag style={{ fontSize: 16 }} /> Tags</div>
+            </div>
+            <div id='sidebar__sub-links'>
+                <div><FaBookmark style={{ fontSize: 16 }} /> Saves</div>
+            </div>
+        </div>
+    )
 }
