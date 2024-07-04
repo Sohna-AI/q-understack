9b956bf622a618b49d587dfbe3f765a0039eb3c8
Ocean Briggs-Mineart <Forestbriggs14@gmail.com>
2024-07-03 17:58:03 -0700

    Styled/added to Navigation component, restyled Footer links

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -1,18 +1,33 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, redirect } from "react-router-dom";
 import ProfileButton from "./ProfileButton";
 import "./Navigation.css";
 
 function Navigation() {
     return (
-        <ul id="navbar">
-            <li>
-                <NavLink to="/">Home</NavLink>
-            </li>
-
-            <li>
-                <ProfileButton />
-            </li>
-        </ul>
+        <>
+            <div id="navbar">
+                <div id="logo_about_links">
+                    <p
+                        id="logo"
+                        onClick={() => redirect('/')}
+                    >LOGO PLACEHOLDER</p>
+                    <NavLink to='/about'>About</NavLink>
+                </div>
+                <div>
+                    <input
+                        type="search"
+                        name="search_bar"
+                        id="search_bar"
+                        placeholder='Search...'
+                    />
+                </div>
+                <div id="home_profile_links">
+                    <NavLink to='/'>Home</NavLink>
+                    <ProfileButton />
+                </div>
+            </div>
+            <div className="split"></div>
+        </>
     );
 }
 
--- react-vite/src/components/Navigation/OpenModalMenuItem.jsx
+++ react-vite/src/components/Navigation/OpenModalMenuItem.jsx
@@ -15,7 +15,7 @@ function OpenModalMenuItem({
     };
 
     return (
-        <li onClick={onClick}>{itemText}</li>
+        <li className='modal_menu_item' onClick={onClick}>{itemText}</li>
     );
 }
 
