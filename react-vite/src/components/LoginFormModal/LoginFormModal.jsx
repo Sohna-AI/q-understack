e5142e77073d71285fcab3ad49ca221f68173378
Ocean Briggs-Mineart <Forestbriggs14@gmail.com>
2024-07-03 19:29:16 -0700

    styled login/signup modal, added error functionality

--- react-vite/src/components/LoginFormModal/LoginFormModal.jsx
+++ react-vite/src/components/LoginFormModal/LoginFormModal.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { thunkLogin } from "../../redux/session";
 import { useDispatch } from "react-redux";
 import { useModal } from "../../context/Modal";
+import { FaEye, FaEyeSlash } from 'react-icons/fa'
 import "./LoginForm.css";
 
 function LoginFormModal() {
@@ -9,10 +10,13 @@ function LoginFormModal() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [errors, setErrors] = useState({});
+    const [showPassword, setShowPassword] = useState(false)
     const { closeModal } = useModal();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+
+        setErrors({});
 
         const serverResponse = await dispatch(
             thunkLogin({
@@ -28,10 +32,18 @@ function LoginFormModal() {
         }
     };
 
+    const handleDemoUserLogin = () => {
+        setErrors({});
+        return dispatch(thunkLogin('demo@aa.io', 'password'))
+            .then(() => {
+                closeModal();
+            });
+    }
+
     return (
         <>
             <h1>Log In</h1>
-            <form onSubmit={handleSubmit}>
+            <form className="login-signup-form" onSubmit={handleSubmit}>
                 <label>
                     Email
                     <input
@@ -41,18 +53,33 @@ function LoginFormModal() {
                         required
                     />
                 </label>
-                {errors.email && <p>{errors.email}</p>}
+                <div className="login-error__container">
+                    {errors.email && <p className="errors">{errors.email}</p>}
+                </div>
                 <label>
                     Password
-                    <input
-                        type="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        required
-                    />
+                    <span className="password-container">
+                        <input
+                            type={showPassword ? 'input' : 'password'}
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            required
+                        />
+                        {showPassword ?
+                            <FaEye onClick={() => setShowPassword(false)} /> :
+                            <FaEyeSlash onClick={() => setShowPassword(true)} />}
+                    </span>
                 </label>
-                {errors.password && <p>{errors.password}</p>}
+                <div className="login-error__container">
+                    {errors.password && <p className="errors">{errors.password}</p>}
+                </div>
                 <button type="submit">Log In</button>
+                <a
+                    id="demo-user-login"
+                    onClick={handleDemoUserLogin}
+                >
+                    Log in as Demo User
+                </a>
             </form>
         </>
     );
