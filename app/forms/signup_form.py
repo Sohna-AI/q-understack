e117121d94e4cd8d83f83542f7b82f619501e447
Ocean Briggs-Mineart <Forestbriggs14@gmail.com>
2024-07-08 17:17:08 -0700

    fixed signup form to validate email

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, EmailField
 from wtforms.validators import DataRequired, Email, ValidationError, Length
 from app.models import User
 
@@ -23,5 +23,5 @@ def username_exists(form, field):
 class SignUpForm(FlaskForm):
     username = StringField(
         'username', validators=[DataRequired(), username_exists])
-    email = StringField('email', validators=[DataRequired(), user_exists])
+    email = EmailField('email', validators=[DataRequired(), Email(), user_exists])
     password = StringField('password', validators=[DataRequired(), Length(min=8)])
