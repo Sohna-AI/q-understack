4400c0f22b02f41a92b7312e6d1b417b2aa60ae5
Big-Hdub <hdub-bb@hotmail.com>
2024-07-08 21:31:06 -0700

    creating test for backend answer creation route

--- react-vite/src/components/TestAnswer/TestAnswer.jsx
+++ react-vite/src/components/TestAnswer/TestAnswer.jsx
@@ -1,10 +1,27 @@
-function Answer(){
-    
-    return (
-        <>
-        <h1>Answer</h1>
+import { useEffect, useState } from "react"
 
-        <form action="" method="post"></form>
-        </>
+function TestAnswer() {
+    const [response, setResponse] = useState({})
+
+    useEffect(() => {
+        const newAnswer = async () => {
+            const data = await fetch("/api/questions/1/answers",
+                {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        text: "hello"
+                    })
+                })
+            setResponse(data)
+        }
+        newAnswer()
+    }, [])
+
+
+    return (<>
+        {if response?.text && <h1>{response}</h1>}
+    </>
     )
 }
+
+export default TestAnswer;
--- react-vite/src/router/index.jsx
+++ react-vite/src/router/index.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 import LoginFormPage from '../components/LoginFormPage';
 import SignupFormPage from '../components/SignupFormPage';
+import TestAnswer from '../components/TestAnswer'
 import Layout from './Layout';
 import LandingPage from '../components/LandingPage';
 import QuestionListPage from '../components/QuestionListPage';
@@ -10,15 +11,28 @@ export const router = createBrowserRouter([
         element: <Layout />,
         children: [
             {
-                path: "/",
+                path: "",
                 element: <LandingPage />,
             },
             {
                 path: 'questions',
-                element: <QuestionListPage />,
                 children: [
                     {
-                        path: 'answers'
+                        path: "",
+                        element: <QuestionListPage />,
+                    },
+                    {
+                        path: ':question_id',
+                        children: [
+                            {
+                                path: "",
+                                element: <h1>question page</h1>
+                            },
+                            {
+                                path: "answers",
+                                element: <TestAnswer />
+                            }
+                        ]
                     }
                 ]
             },
