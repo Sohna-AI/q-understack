5fea1f199a68bb04222314c7e7d8cc83ee0d9549
Pushpinder Singh <142683189+Sohna-AI@users.noreply.github.com>
2024-07-11 17:15:41 -0700

    changed the comment form's comment length to 1500

--- app/forms/create_comment_form.py
+++ app/forms/create_comment_form.py
@@ -3,4 +3,4 @@ from wtforms.fields import TextAreaField
 from wtforms.validators import DataRequired, Length
 
 class CommentForm(FlaskForm):
-    comment = TextAreaField('Comment', validators=[DataRequired(), Length(max=255)])
+    comment = TextAreaField('Comment', validators=[DataRequired(), Length(max=1500)])
