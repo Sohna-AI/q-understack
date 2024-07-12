d640b2c2b51d8b78beb14263dad27801bd070ee9
Pushpinder Singh <142683189+Sohna-AI@users.noreply.github.com>
2024-07-12 13:30:42 -0700

    created redux store for user created questions and user saved questions to handle deleting and querying to display. updated reducers as needed. created react components for user's questions and user's saved questions. implement css with both pages. created a nav bar to navigate between user's questions and saved questions.

--- react-vite/src/router/index.jsx
+++ react-vite/src/router/index.jsx
@@ -5,42 +5,58 @@ import Layout from './Layout';
 import LandingPage from '../components/LandingPage';
 import QuestionListPage from '../components/QuestionListPage';
 import QuestionDetailPage from '../components/QuestionDetailPage';
+import SavedQuestions from '../components/SavesPage/SavesPage';
+import UserQuestions from '../components/UserQuestions/UserQuestions';
+import UserQuestionSaveNav from '../components/UserQuestionSavePage/UserQuestionSaveNav';
 // import TestAnswer from '../components/TestAnswer/TestAnswer';
 
 export const router = createBrowserRouter([
-    {
-        element: <Layout />,
+  {
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <LandingPage />,
+      },
+      {
+        path: 'questions',
         children: [
-            {
-                path: "/",
-                element: <LandingPage />,
-            },
-            {
-                path: 'questions',
-                children: [
-                    {
-                        index: true,
-                        element: <QuestionListPage />
-                    },
-                    {
-                        path: ':questionId',
-                        element: <QuestionDetailPage />
-                    }
-
-                ]
-            },
-            {
-                path: "login",
-                element: <LoginFormPage />,
-            },
-            {
-                path: "signup",
-                element: <SignupFormPage />,
-            },
-            // {
-            //     path: "test",
-            //     element: <TestAnswer />
-            // }
+          {
+            index: true,
+            element: <QuestionListPage />,
+          },
+          {
+            path: ':questionId',
+            element: <QuestionDetailPage />,
+          },
         ],
-    },
+      },
+      {
+        path: 'user',
+        element: <UserQuestionSaveNav />,
+        children: [
+          {
+            path: 'saves',
+            element: <SavedQuestions />,
+          },
+          {
+            path: 'questions',
+            element: <UserQuestions />,
+          },
+        ],
+      },
+      {
+        path: 'login',
+        element: <LoginFormPage />,
+      },
+      {
+        path: 'signup',
+        element: <SignupFormPage />,
+      },
+      // {
+      //     path: "test",
+      //     element: <TestAnswer />
+      // }
+    ],
+  },
 ]);
