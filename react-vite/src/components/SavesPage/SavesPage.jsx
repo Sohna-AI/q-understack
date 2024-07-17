21667ccea990b267c67d72103d554e7dfa639ff2
Pushpinder Singh <142683189+Sohna-AI@users.noreply.github.com>
2024-07-17 10:53:51 -0700

    working on fix for method not allowed for unsaving answer on savespage

--- react-vite/src/components/SavesPage/SavesPage.jsx
+++ react-vite/src/components/SavesPage/SavesPage.jsx
@@ -67,7 +67,7 @@ const SavedQuestions = () => {
                         </div>
                         <div>
                           <OpenModalButton
-                            modalComponent={<UnsaveQuestionModal questionId={questions.data[id]} />}
+                            modalComponent={<UnsaveQuestionModal questionId={questions.data[id].id} />}
                           >
                             <svg viewBox="0 0 384 512" height="1em" xmlns="http://www.w3.org/2000/svg">
                               <path d="M0 48C0 21.5 21.5 0 48 0l0 48V441.4l130.1-92.9c8.3-6 19.6-6 27.9 0L336 441.4V48H48V0H336c26.5 0 48 21.5 48 48V488c0 9-5 17.2-13 21.3s-17.6 3.4-24.9-1.8L192 397.5 37.9 507.5c-7.3 5.2-16.9 5.9-24.9 1.8S0 497 0 488V48z"></path>
@@ -126,7 +126,9 @@ const SavedQuestions = () => {
                           </p>
                         </div>
                         <div>
-                          <OpenModalButton modalComponent={<UnsaveAnswerModal answerId={answers.data[id]} />}>
+                          <OpenModalButton
+                            modalComponent={<UnsaveAnswerModal answerId={answers.data[id].id} />}
+                          >
                             <svg viewBox="0 0 384 512" height="1em" xmlns="http://www.w3.org/2000/svg">
                               <path d="M0 48C0 21.5 21.5 0 48 0l0 48V441.4l130.1-92.9c8.3-6 19.6-6 27.9 0L336 441.4V48H48V0H336c26.5 0 48 21.5 48 48V488c0 9-5 17.2-13 21.3s-17.6 3.4-24.9-1.8L192 397.5 37.9 507.5c-7.3 5.2-16.9 5.9-24.9 1.8S0 497 0 488V48z"></path>
                             </svg>
