6fcb809ce6f50a02f9f0767b78cdd20e550f290a
Big-Hdub <hdub-bb@hotmail.com>
2024-07-21 14:36:58 -0700

    fixed css so edit answer svgs don't show in update answer modal,  don't show bookmark when not logged in,  added logic to verify there is a session user if a bookmark action happens

--- react-vite/src/components/QuestionDetailPage/QuestionDetailPage.jsx
+++ react-vite/src/components/QuestionDetailPage/QuestionDetailPage.jsx
@@ -1,15 +1,17 @@
 import {
-  thunkPostAnswer,
-  thunkGetQuestionDetailsById,
-  thunkSaveQuestion,
-  thunkUnsaveQuestion,
-  thunkSaveAnswer,
-  thunkUnsaveAnswer,
+  thunkPostAnswer, thunkGetQuestionDetailsById, thunkSaveQuestion,
+  thunkUnsaveQuestion, thunkSaveAnswer, thunkUnsaveAnswer,
 } from '../../utils/store';
+import {
+  FaArrowAltCircleDown, FaArrowAltCircleUp, FaRegArrowAltCircleDown,
+  FaRegArrowAltCircleUp, FaBookmark, FaRegBookmark
+} from 'react-icons/fa';
+import { NavLink, useNavigate, useParams } from 'react-router-dom';
+import OpenModalMenuItem from '../Navigation/OpenModalMenuItem';
+import UpdateAnswerModal from '../UpdateAnswer/UpdateAnswer';
 import * as questionActions from '../../redux/questions';
 import * as commentActions from '../../redux/comments';
 import { useDispatch, useSelector } from 'react-redux';
-import { NavLink, useNavigate, useParams } from 'react-router-dom';
 import * as answerActions from '../../redux/answers';
 import OpenModalButton from '../OpenModalButton';
 import SignupFormModal from '../SignupFormModal';
@@ -19,16 +21,6 @@ import LoginFormModal from '../LoginFormModal';
 import { useEffect, useState } from 'react';
 import './QuestionDetailPage.css';
 
-import {
-  FaArrowAltCircleDown,
-  FaArrowAltCircleUp,
-  FaRegArrowAltCircleDown,
-  FaRegArrowAltCircleUp,
-  FaBookmark,
-  FaRegBookmark,
-} from 'react-icons/fa';
-import UpdateAnswerModal from '../UpdateAnswer/UpdateAnswer';
-import OpenModalMenuItem from '../Navigation/OpenModalMenuItem';
 
 export default function QuestionDetailPage() {
   const { questionId } = useParams();
@@ -73,19 +65,27 @@ export default function QuestionDetailPage() {
   };
 
   const handleQuestionSave = async () => {
-    dispatch(thunkSaveQuestion(questionId))
+    if (sessionUser) {
+      dispatch(thunkSaveQuestion(questionId))
+    }
   };
 
   const handleQuestionUnsave = async () => {
-    dispatch(thunkUnsaveQuestion(questionId))
+    if (sessionUser) {
+      dispatch(thunkUnsaveQuestion(questionId))
+    }
   };
 
   const handleAnswerSave = async (questionId, answerId) => {
-    dispatch(thunkSaveAnswer(questionId, answerId))
+    if (sessionUser) {
+      dispatch(thunkSaveAnswer(questionId, answerId))
+    }
   };
 
   const handleAnswerUnsave = async (questionId, answerId) => {
-    dispatch(thunkUnsaveAnswer(questionId, answerId))
+    if (sessionUser) {
+      dispatch(thunkUnsaveAnswer(questionId, answerId))
+    }
   };
 
   return (
@@ -108,14 +108,17 @@ export default function QuestionDetailPage() {
             </div>
             <div id="main-content">
               <div id="vote-area">
-                {question.user_vote === true ? <FaArrowAltCircleUp /> : <FaRegArrowAltCircleUp />}
+                {(sessionUser && question.user_vote === true) ? <FaArrowAltCircleUp /> : <FaRegArrowAltCircleUp />}
                 <p>{question.num_votes}</p>
-                {question.user_vote === false ? <FaArrowAltCircleDown /> : <FaRegArrowAltCircleDown />}
-                {question.user_save ? (
-                  <FaBookmark onClick={handleQuestionUnsave} />
-                ) : (
-                  <FaRegBookmark onClick={handleQuestionSave} />
-                )}
+                {(sessionUser && question.user_vote === false) ? <FaArrowAltCircleDown /> : <FaRegArrowAltCircleDown />}
+                {sessionUser && <>
+                  {question.user_save ? (
+                    <FaBookmark onClick={handleQuestionUnsave} />
+                  ) : (
+                    <FaRegBookmark onClick={handleQuestionSave} />
+                  )}
+                </>
+                }
               </div>
               <div id="question-details__container">
                 <p>{question.details}</p>
@@ -159,18 +162,20 @@ export default function QuestionDetailPage() {
                   return (
                     <div className="answer__container" key={`answer-${answerId}`}>
                       <div className='flex gap-15'>
-                        {answer.user_save ? (
-                          <FaBookmark onClick={() => handleAnswerUnsave(question.id, answer.id)} />
-                        ) : (
-                          <FaRegBookmark onClick={() => handleAnswerSave(question.id, answer.id)} />
-                        )}
+                        {sessionUser && <>
+                          {answer.user_save ? (
+                            <FaBookmark onClick={() => handleAnswerUnsave(question.id, answer.id)} />
+                          ) : (
+                            <FaRegBookmark onClick={() => handleAnswerSave(question.id, answer.id)} />
+                          )}
+                        </>}
                         <p>
                           {answer.text} - {answer.user.username}
                         </p>
                         {sessionUser?.id === answer.user_id && (
                           <OpenModalButton
                             buttonText={
-                              <svg height="1em" viewBox="0 0 512 512">
+                              <svg style={{ zIndex: "0" }} height="1em" viewBox="0 0 512 512">
                                 <path d="M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1v32c0 8.8 7.2 16 16 16h32zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z"></path>
                               </svg>
                             }
@@ -215,6 +220,6 @@ export default function QuestionDetailPage() {
         }
       </>}
       {!isLoaded && <div style={{ height: '100vw' }} id="question-card__container"></div>}
-    </div>
+    </div >
   );
 }
